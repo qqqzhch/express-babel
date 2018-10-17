01979ca87a7c2561b8fb9497969a104d3b8fbf73
zhangchao <544262408@qq.com>
2018-10-17 11:47:52 +0800

    多语言翻译调用百度

---
+++ src/data/en.js
@@ -0,0 +1,60 @@
+export default {
+    head: {
+        menu1:'COMMUNITIY',
+        menu2:'Create eos account',
+        menu3:'Chrome plugin',
+        menu4:'Buy Eos',
+        menu5:'HOW TO PLAY',
+        menu6:'LOGIN',
+        menu7:'Invite',
+    },
+    panel:{
+        title1:'BET AMOUNT',
+        title2:'PAYOUT ON WIN',
+        title3:'ROLL UNDER TO WIN',
+        title4:'PAYOUT',
+        title5:'WIN CHANCE',
+        title6:'LOGIN',
+        title7:'ROLL DICE'
+
+    },
+    order:{
+        title1:'ALL BETS',
+        title2:'Time',
+        title3:'Bettor',
+        title4:'Roll Under',
+        title5:'Bet',
+        title6:'Roll'
+    },
+    play:{
+        title1:'1. Make sure you have an EOS account. For more information on how to create one,',
+        title2:'2. If you haven’t already, download and install Chrome plugin, an EOS wallet that facilitates interaction between users and dApps',
+        title3:'3. Set your BET AMOUNT. This is the amount of EOS you will be wagering, If you don\'t have EOS,  can buy some EOS',
+        title4:'4. Adjust the slider to change your chance of winning.',
+        title5:'5. Click ROLL DICE to place your bet.',
+        title6:'6. If your number is lower than your ROLL UNDER TO WIN number, you win!',
+        title7:'7. If you get a notice that your transaction failed, please check that you have enough CPU & bandwidth to make the transaction! Please use EOSToolkit to make any changes to your account!',
+        title8:'You can view your EOS balance next to the ROLL DICE button. The table below the slider bar shows recent bets from all players across the world.',
+        title9:'Still have questions? Reach out to us at Discord and we’ll be happy to help!',
+        title10:'click here'
+
+    },
+    doAction:{
+        title1:'Bet Failed',
+        title2:'Bet Amount should not be more than ',
+        title3:'Bet Amount should be more than ',
+        title4:'Waiting for Scatter to confirm transfer...',
+        title5:'Bet success',
+        title6:'Waiting for bet result',
+        title7:'Waiting for bet result'
+    },
+    invitel:{
+        title1:'Invite friends for Rewards!',
+        title2:'Copy',
+        title3:'Rule',
+        title4:'When you invite your friend A, you will receive a 0.4% bonus for each time a friend A bets.',
+        title5:'When a friend invites friend B again, you will also receive a 0.2% bet bonus from friend B.',
+
+    }
+
+  }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,99 @@
-import app from './app';
+// import app from './app';
 
-const { PORT = 8080 } = process.env;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
+// const { PORT = 8080 } = process.env;
+// app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
+
+import logger from './log'
+
+import fetch from 'cross-fetch';
+var md5 = require('md5');
+
+import endata from './data/cn';
+import fs from 'fs';
+
+
+async function demo(){
+    // var  result= await translate('苹果','cht')
+    // console.log(result)
+
+    for(var key in endata){    
+        for(var key2 in endata[key]){    
+            
+            var  result= await translate(endata[key][key2],'cht'); //'cht'  'zh'
+            if(result){
+                endata[key][key2]=result;
+            }
+            console.log(endata[key]);
+        
+        }   
+    }
+    fs.writeFileSync('dist.json',JSON.stringify(endata));  
+    
+}
+demo()
+
+ 
+
+  async function translate(query,to){
+    var appid = '20181015000219404';
+    var key = 'M_RgFuOPd3EdNbTxHeYZ';
+    var salt = (new Date).getTime();
+    // var query = 'apple';
+    // 多个query可以用\n连接  如 query='apple\norange\nbanana\npear'
+    var from = 'zh'; //'en'
+    // var to = 'zh';
+    var str1 = appid + query + salt + key;
+    console.log('str1',str1)
+    var sign = md5(str1);
+    console.log('sign',sign)
+    if(query=='苹果'){
+        console.log(1)
+    }
+
+
+
+    try {
+     var url=`http://api.fanyi.baidu.com/api/trans/vip/translate?from=${from}&to=${to}&q=${encodeURI(query)}&appid=${appid}&salt=${salt}&sign=${sign}`;
+     console.log(url);
+      const res = await fetch(url);
+      
+      if (res.status >= 400) {
+        throw new Error("Bad response from server");
+      }
+      
+      const user = await res.json();
+      console.log(user);
+      if(user.error_code!=undefined){
+          return false
+      }else{
+          return user.trans_result[0].dst;
+      }
+    //   logger.info(user.trans_result);
+     
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+
+
+
+//   $.ajax({
+//       url: 'http://api.fanyi.baidu.com/api/trans/vip/translate',
+//       type: 'get',
+//       dataType: 'jsonp',
+//       data: {
+//           q: query,
+//           appid: appid,
+//           salt: salt,
+//           from: from,
+//           to: to,
+//           sign: sign
+//       },
+//       success: function (data) {
+//           console.log(data);
+//       } 
+//   });
+
+
